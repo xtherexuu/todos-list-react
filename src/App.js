710924c40f7cf37843a628a5a67d150922192629
xtherexuu <therexuu@gmail.com>
2022-12-12 14:19:45 +0100

    add deleting task interaction

--- src/App.js
+++ src/App.js
@@ -6,35 +6,36 @@ import Form from "./Form";
 import Buttons from "./Buttons";
 import Tasks from "./Tasks";
 
-const tasks = [
-    { id: 1, task: "Pozmywać naczynia", done: true },
-    { id: 2, task: "Posprzątać pokój", done: false },
-];
-
 function App() {
     const [hiddenTaskStatus, setHiddenTaskStatus] = React.useState(false);
 
+    let [tasks, setTasks] = React.useState([
+        { id: 1, task: "Pozmywać naczynia", done: true },
+        { id: 2, task: "Posprzątać pokój", done: false },
+    ]);
+
     const toggleHiddenTaskStatus = () => {
         setHiddenTaskStatus((hiddenTaskStatus) => !hiddenTaskStatus);
+    };
+
+    const removeTask = (id) => {
+        setTasks((tasks) => (tasks = [...tasks.slice(0, id - 1), ...tasks.slice(id)]));
     };
 
     return (
         <Wrapper>
             <Header title="Lista zadań" />
             <Section title="Dodaj nowe zadanie" body={<Form />} />
-            <Section 
-                title="Lista zadań" 
+            <Section
+                title="Lista zadań"
                 extraContent={
-                    <Buttons 
-                    tasks={tasks}
-                    hiddenTaskStatus = {hiddenTaskStatus} 
-                    toggleHiddenTaskStatus={toggleHiddenTaskStatus} />
-                } 
-                body={
-                    <Tasks 
-                    tasks={tasks} 
-                    hiddenTaskStatus={hiddenTaskStatus} />
-                } 
+                    <Buttons
+                        tasks={tasks}
+                        hiddenTaskStatus={hiddenTaskStatus}
+                        toggleHiddenTaskStatus={toggleHiddenTaskStatus}
+                    />
+                }
+                body={<Tasks tasks={tasks} hiddenTaskStatus={hiddenTaskStatus} removeTask={removeTask} />}
             />
         </Wrapper>
     );
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./style.css";
 
-const Tasks = ({tasks, hiddenTaskStatus}) => (
+const Tasks = ({ tasks, hiddenTaskStatus, removeTask}) => (
     <ul className="list">
         {tasks.map((task) => (
             <li key={task.id} className={`list__item ${task.done && hiddenTaskStatus ? "list__item--hidden" : ""}`}>
                 <button className="list__button list__button--checking">{task.done ? "✔" : ""}</button>
                 <span className={`list__taskContent ${task.done ? "list__item--done" : ""}`}>{task.task}</span>
-                <button className="list__button list__button--deleting">🗑</button>
+                <button onClick={() => removeTask(task.id)} className="list__button list__button--deleting">
+                    🗑
+                </button>
             </li>
         ))}
     </ul>
