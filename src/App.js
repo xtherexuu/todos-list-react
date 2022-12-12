e0691024a75b2a37c269705f48dba0846e7ed806
xtherexuu <therexuu@gmail.com>
2022-12-12 13:10:34 +0100

    toggleHiddenTaskStatus interaction

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Header from "./Header";
 import Wrapper from "./Wrapper";
 import Section from "./Section";
@@ -10,17 +11,30 @@ const tasks = [
     { id: 2, task: "Posprzątać pokój", done: false },
 ];
 
-const hiddenTaskStatus = false;
-
 function App() {
+    const [hiddenTaskStatus, setHiddenTaskStatus] = React.useState(false);
+
+    const toggleHiddenTaskStatus = () => {
+        setHiddenTaskStatus((hiddenTaskStatus) => !hiddenTaskStatus);
+    };
+
     return (
         <Wrapper>
             <Header title="Lista zadań" />
             <Section title="Dodaj nowe zadanie" body={<Form />} />
             <Section 
-              title="Lista zadań" 
-              extraContent={<Buttons tasks={tasks} />} 
-              body={<Tasks tasks={tasks} hiddenTaskStatus={hiddenTaskStatus} />} 
+                title="Lista zadań" 
+                extraContent={
+                    <Buttons 
+                    tasks={tasks}
+                    hiddenTaskStatus = {hiddenTaskStatus} 
+                    toggleHiddenTaskStatus={toggleHiddenTaskStatus} />
+                } 
+                body={
+                    <Tasks 
+                    tasks={tasks} 
+                    hiddenTaskStatus={hiddenTaskStatus} />
+                } 
             />
         </Wrapper>
     );
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,8 +1,8 @@
 import "./style.css";
 
-const Buttons = ({tasks}) => (
+const Buttons = ({tasks, hiddenTaskStatus, toggleHiddenTaskStatus}) => (
     <div className={`buttonsContainer ${tasks.length ? "" : "buttonsContainer--hidden"}`}>
-        <button className="buttons__button">Ukryj ukończone</button>
+        <button className="buttons__button" onClick={toggleHiddenTaskStatus}>{hiddenTaskStatus ? "Pokaż" : "Ukryj"}  ukończone</button>
         <button disabled={tasks.every((task) => task.done)} className="buttons__button">
             Zaznacz wszystkie
         </button>
